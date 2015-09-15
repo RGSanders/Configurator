2b8586701125813939508cfeb2d26a8031ca145a
Patrick Cool <pcool@foxthermalinstruments.com>
2015-09-15 16:37:43 -0700

    Update 9/15
    Added object keys, removed test code.

--- js/configurator.js
+++ js/configurator.js
@@ -12,3 +12,30 @@ Meter.prototype = {
 function Meter(model,inline) = {
 
 }
+var Meter = {
+	
+	model: ,
+	
+	gas: ,
+	
+	shape: ,
+
+	size_units: ,
+
+	probe_size: ,
+
+	stp: ,
+
+	
+
+	init: function(){
+
+	},
+	calculateDensity: function(){
+
+	},
+	calculateSize: function(){
+
+	}
+
+};
